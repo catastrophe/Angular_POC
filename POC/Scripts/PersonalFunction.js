a88631c73cd6c27a7f157ceb01eb0b62420ea244
Sidharth Rath <m337dal337@hotmail.com>
2015-10-10 21:30:16 +0530

    Initial Commit

---
+++ POC/Scripts/PersonalFunction.js
@@ -0,0 +1,8 @@
+﻿var app = angular.module('myApp', []);
+app.controller('myPersonalFunction', function ($scope) {
+    $scope.firstname = "Sid";
+    $scope.lastname = "Rath";
+    $scope.fullname = function () {
+        return $scope.firstname + " " + $scope.lastname;
+    }
+});
